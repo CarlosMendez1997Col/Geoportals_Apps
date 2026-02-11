85a4fc181bd9fedce2b00900c2a214c8e91c926b
Carlos Mendez <carlos.mendez@unesp.br>
2026-02-11 09:57:15 -0500

    update

--- src/components/MapView.vue
+++ src/components/MapView.vue
@@ -1,1 +1,18 @@
+<template>
+  <div id="map" style="height:500px; width:100%;"></div>
+</template>
+
+<script>
+import L from 'leaflet';
+
+export default {
+  name: 'MapView',
+  mounted() {
+    const map = L.map('map').setView([4.711, -74.072], 6); // Bogotá
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© OpenStreetMap contributors'
+    }).addTo(map);
+  }
+};
+</script>
 
